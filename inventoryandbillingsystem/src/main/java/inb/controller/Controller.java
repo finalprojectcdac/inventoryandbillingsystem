7f15af399206e8bba43c4835cea8a8a73ca4b2bb
Vaibhav Chauhan <chauhanvaibhav095@gmail.com>
2021-03-15 15:57:42 +0530

    test

--- inventoryandbillingsystem/src/main/java/inb/controller/Controller.java
+++ inventoryandbillingsystem/src/main/java/inb/controller/Controller.java
@@ -54,13 +54,14 @@ public class Controller
 	@PutMapping("/saveinventorypage") // created by vaibhav// final done
 	public CResult addAllItems(@RequestBody List<Inventory> lsi)
 	{
+		System.out.println("save inventory called");
 		CResult x = s.addMultipleItems(lsi);
 		return x;
 	}
 	
 	@PutMapping("/saveSupplierRecordFromInventorypage")// final done
 	public CResult addSupplier(@RequestBody SupplierRecord a){
-		
+		System.out.println("save supplier called");
 		CResult x=s.RegisterSupplier(a);
 		return x;
 	}
@@ -68,6 +69,7 @@ public class Controller
 	@GetMapping("/getitemdetailsfrominventorytable")
 	public CResult getItemDetails(@RequestParam String item_code) {
 		//we have to make the function call here
+		System.out.println(item_code);
 		CResult x=s.getItem(item_code);
 	return x;
 	}
